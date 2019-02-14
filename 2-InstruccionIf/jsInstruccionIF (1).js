d58a670c2644268eb2d176aecaefe778e2efe183
DiegoJabie1 <47386362+DiegoJabie1@users.noreply.github.com>
2019-02-14 11:56:10 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,8 @@
 function Mostrar()
 {
 //tomo la edad  
-
+var edad
+edad= parseInt(document.getElementById(edad).va)
 
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,9 +1,21 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
+edad=parseInt(document.getElementById("edad").value)
 
-
-
+if(edad>=18)
+{
+alert("Usted es mayor de edad.");
+}
+else if(edad>=13)
+{
+alert("Usted es adolescente.");
+}
+else
+{
+alert("Usted es un niño.");
+}
 
 
 
