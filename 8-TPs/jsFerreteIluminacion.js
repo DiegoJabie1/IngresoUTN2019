8571d69005ddfff452e9d2d5fb05dc83ee496d1f
DiegoJabie1 <47386362+DiegoJabie1@users.noreply.github.com>
2019-02-20 12:04:16 -0300

    Add files via upload

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,74 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+var precio = 35;
+var marca = (document.getElementById("Marca").value);
+var cantidad= parseInt(document.getElementById("Cantidad").value);
+var descuento;
+var precioConDescuento;
+var precioFinal;
+var iibb;
+
+if (cantidad >= 6)
+{
+descuento=precio*50/100;
 }
+else if (cantidad==5)
+{
+   if(marca == "ArgentinaLuz")
+   {
+       descuento=precio*40/100;
+   }
+   else
+   {
+       descuento=precio*30/100;
+   }
+}
+else if (cantidad==4)
+{
+   if(marca=="ArgentinaLuz"||marca=="FelipeLamparas")
+   {
+       descuento=precio*25/100;
+   }
+   else
+   {
+       descuento=precio*20/100;
+   }
+}
+else if (cantidad==3)
+{
+    if(marca=="ArgentinaLuz")
+    {
+        descuento=precio*15/100;
+    }
+    else if(marca=="FelipeLamparas")
+        {
+            descuento=precio*10/100;
+        }
+        else
+        {
+            descuento=precio*5/100;
+        }
+    }
+else
+{
+descuento=0
+}
+precioConDescuento=precio-descuento;
+
+document.getElementById("precioDescuento").value=precioConDescuento;
+
+precioFinal=cantidad*precioConDescuento;
+
+if(precioFinal>120)
+{
+iibb=precioFinal*10/100;
+precioFinal=precioFinal+iibb;
+alert("Su importe a pagar es de $"+precioFinal+" con un IIBB agregado de $"+iibb+".");
+}
+else
+{
+alert("Su importe a pagar es de $"+precioFinal+".");
+}
+}
+
